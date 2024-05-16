68505096cd313c5430ab207a80bcc0a00f47cfff
Fabian Hadrych <fabian.hadrych@hcu-hamburg.de>
2024-05-16 21:53:50 +0200

    Main file hinzugefügt

---
+++ main.py
@@ -0,0 +1,7 @@
+
+# Eingabeaufforderung
+print(f'Bitte ein Land eingeben (Deutschsprachig):')
+
+# Eingabe des Landes (Deutschsprachig)
+eingabe = input()
+print(f'Die folgenden Berechnungen werden für {eingabe} durchgeführt!')
